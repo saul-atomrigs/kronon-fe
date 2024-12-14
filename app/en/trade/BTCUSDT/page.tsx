bfe4f3505bb18e2b9b88290b15d4f0bb1a82e161
Saul Lee <solleedata@gmail.com>
2024-12-14 22:58:04 +0900

    Integrate BuySellTable component and enable price selection in OrderBookTable

--- app/en/trade/BTCUSDT/page.tsx
+++ app/en/trade/BTCUSDT/page.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 import CurrentInfoTable from '@/app/components/CurrentInfoTable';
 import OrderBookComponent from '@/app/components/OrderBookTable';
+import BuySellTable from '@/app/components/BuySellTable';
 
-export default function page() {
+export default function Page() {
+  const [selectedPrice, setSelectedPrice] = useState<number>(0);
+
   return (
     <>
-      <CurrentInfoTable />;
-      <OrderBookComponent />;
+      <CurrentInfoTable />
+      <OrderBookComponent onPriceSelect={setSelectedPrice} />
+      <BuySellTable selectedPrice={selectedPrice} />
     </>
   );
 }
